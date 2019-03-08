128afd8aed846300637cb3043d141f60c65156e0
unknown <tarasikuss@gmail.com>
2019-03-08 15:35:27 +0300

    fix: complete task

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var quantity = 0;
+  for (var i=0; i<preferences.length; i++){
+	  var firstElem = preferences[i];
+	  var secondElem = preferences[firstElem-1];
+	  var thirdElem = preferences[secondElem-1];
+  	if (firstElem!==secondElem&&secondElem!==thirdElem&&firstElem!==thirdElem&&thirdElem===(i+1)){
+  		quantity++;
+  	}
+  }
+  return quantity/3;
 };
